9b31b11d9c970199e6b18e9ce9ad192ed47190de
Denis Borisovich Fishman <106389581+fishman123456@users.noreply.github.com>
2024-04-18 12:33:33 +0700

    вкыпе

---
+++ CheckDateWork.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+namespace Autocad_Get_layer_18_04_2024
+{
+    // класс для проверки текущей даты
+    public static class CheckDateWork
+    {
+        public static void CheckDate()
+        {
+            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+            DateTime dt1 = DateTime.Now;
+            DateTime dt2 = DateTime.Parse("20/05/2024");
+           
+
+            if (dt1.Date > dt2.Date)
+            {
+                ed.WriteMessage("Your Application is Expire");
+                // Выход из проложения добавил 01-01-2024. Чтобы порядок был....
+                Application.ShowAlertDialog("1 Save your drawings !!!");
+                Application.ShowAlertDialog("2 Save your drawings !!!");
+                Application.ShowAlertDialog("3 Save your drawings !!!");
+                Application.ShowAlertDialog("Autocad Process Kill !!!");
+                // закрытие процесса autocad 09-01-2024
+                foreach (Process Proc in Process.GetProcesses())
+                {
+                    if (Proc.ProcessName.Equals("acad"))
+                    {
+                        Proc.CloseMainWindow();
+                        Proc.Kill();
+                    }
+                }
+                //w1.Close();
+            }
+            else
+            {
+                //MessageBox.Show("Работайте до   " + dt2.ToString());
+            }
+        }
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,91 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autocad_Get_layer_18_04_2024
+{
+    public class Class1
+    {
+        // This method can have any name
+        [CommandMethod("DisplayLayers")]
+        public void TestDisplayLayers()
+        {
+            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+            List<string> info = LayersToList(db);
+            ed.WriteMessage(DateTime.Now.ToString()+"\n" + "кол-во кабеля" + "\n");
+            foreach (string lname in info)
+                ed.WriteMessage(lname+"\n");
+        }
+
+        public List<string> LayersToList(Database db)
+        {
+            // Открываем для редактирования документ
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            List<string> lstlay = new List<string>();
+
+            LayerTableRecord layer;
+            using (Transaction tr = db.TransactionManager.StartOpenCloseTransaction())
+            {
+                LayerTable lt = tr.GetObject(db.LayerTableId, OpenMode.ForRead) as LayerTable;
+                // Создайте список фильтров таким образом, чтобы только
+                // выбираются ссылки на блоки
+                
+                TypedValue[] filList = new TypedValue[] {
+            new TypedValue(Convert.ToInt32(DxfCode.Operator), "<and"),
+            new TypedValue(Convert.ToInt32(DxfCode.Operator), "<or"),
+            new TypedValue(Convert.ToInt32(DxfCode.Start), "POLYLINE"),
+            new TypedValue(Convert.ToInt32(DxfCode.Start), "LWPOLYLINE"),
+            new TypedValue(Convert.ToInt32(DxfCode.Start), "POLYLINE2D"),
+            new TypedValue(Convert.ToInt32(DxfCode.Start), "POLYLINE3d"),
+            new TypedValue(Convert.ToInt32(DxfCode.Operator), "or>"),
+            new TypedValue(Convert.ToInt32(DxfCode.Operator), "and>")
+        };
+                SelectionFilter filter = new SelectionFilter(filList);
+                PromptSelectionOptions opts = new PromptSelectionOptions();
+                opts.MessageForAdding = "Выберите обьекты: ";
+                PromptSelectionResult res = ed.GetSelection(opts, filter);
+                // Do nothing if selection is unsuccessful
+                
+                if (res.Status != PromptStatus.OK)
+                    return lstlay;
+                SelectionSet selSet = res.Value;
+                // добавляем в массив выбранные обьекты
+                ObjectId[] idArray = selSet.GetObjectIds();
+               
+                int countK = 0;
+                // код для создания  слоёв выбранных полилиний
+                foreach (ObjectId id in idArray)
+                {
+                    Entity ent = tr.GetObject(id, OpenMode.ForWrite) as Entity;
+                    countK++;
+                    var  layerW = ent.Layer.ToString();
+                    lstlay.Add(layerW);
+                }
+                lstlay.Insert(0, countK.ToString());
+                // код для создания существующих не замороженных слоёв
+                //foreach (ObjectId layerId in lt)
+                //{
+                //    layer = tr.GetObject(layerId, OpenMode.ForWrite) as LayerTableRecord;
+                //    if (!layer.IsFrozen)
+                //    {
+                //        lstlay.Add(layer.Name);
+                //    }
+
+                //}
+                
+            }
+            // сохраняем в файл
+            SaveCsv saveCsv = new SaveCsv();
+            saveCsv.saveCsv(lstlay);
+            return lstlay;
+        }
+    }
+}
